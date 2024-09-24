a6d214e0c16571ff86064ee29f0033d90f36a2c4
RizkiB12 <rbagus306@gmail.com>
2024-09-24 21:30:08 +0700

    feat: Add google fonts for style the font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    screens: {
+      sm: "340px",
+      md: "540px",
+      lg: "768px",
+      xl: "1180px",
+    },
     extend: {},
+    fontFamily: {
+      Jost: ["Jost", "sans-serif"],
+      Lobster: ["Lobster", "sans-serif"],
+    },
   },
+
   plugins: [],
 };
