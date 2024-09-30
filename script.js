6482eb8a92411d90048bee0cc334f712c63e3418
RizkiB12 <rbagus306@gmail.com>
2024-09-30 23:17:32 +0700

    feat: add review section

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ navLink.forEach((link) => {
     hamburger.classList.toggle("ri-close-large-line");
   });
 });
+
+// Swiper JS
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  speed: 400,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  grabCursor: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
